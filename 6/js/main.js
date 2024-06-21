ce49537b5e27db22a0ffb88eb3557a8adab35779
Keks <keks@htmlacademy.ru>
2024-06-21 21:51:03 +0300

    :heavy_check_mark: Сборка #6

---
+++ 6/js/main.js
@@ -0,0 +1,8 @@
+import { similarPhotos } from './create-photos.js';
+import './edit-img.js';
+import './upload-to-server.js';
+import { renderImg } from './view-img.js';
+import './filter-img.js';
+
+
+renderImg(similarPhotos());
